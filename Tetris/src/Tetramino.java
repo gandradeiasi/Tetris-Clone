b550bf2b6aa041bc780be2c6ca4eef9aa9079d4a
gandradeiasi <31139106+gandradeiasi@users.noreply.github.com>
2017-09-21 21:05:11 -0300

    Add files via upload

---
+++ Tetris/src/Tetramino.java
@@ -0,0 +1,309 @@
+import java.awt.Color;
+import java.awt.Point;
+import java.util.Random;
+
+public class Tetramino{
+	private int		size;
+	private int		bottom;
+	private int 	left;
+	private int 	right;
+	private int[][] matrix;
+	private boolean moved;
+	private Point 	position;
+	private Color	color;
+
+	public Tetramino() {
+		moved = false;
+		
+		Random random = new Random();
+		
+		int minimumColor = 0;
+		int maximumColor = 200;
+		
+		color = new Color(random.nextInt(maximumColor-minimumColor)+minimumColor,random.nextInt(maximumColor-minimumColor)+minimumColor,random.nextInt(maximumColor-minimumColor)+minimumColor);
+		
+		switch(random.nextInt(7)) {
+		case 0:
+			size = 4;
+			position = new Point(3,0);
+			matrix = new int[][] {{0,1,0,0},
+							      {0,1,0,0},
+							      {0,1,0,0},
+								  {0,1,0,0}};
+			break;
+		case 1:
+			size = 3;
+			position = new Point(3,0);
+			matrix = new int[][] {{0,1,0},
+							      {1,1,1},
+							      {0,0,0}};
+			break;
+		case 2:
+			size = 3;
+			position = new Point(3,0);
+			matrix = new int[][] {{0,1,1},
+							      {1,1,0},
+							      {0,0,0}};
+			break;
+		case 3:
+			size = 3;
+			position = new Point(3,0);
+			matrix = new int[][] {{1,1,0},
+							      {0,1,1},
+							      {0,0,0}};
+			break;
+		case 4:
+			size = 3;
+			position = new Point(3,0);
+			matrix = new int[][] {{0,1,0},
+							      {0,1,0},
+							      {0,1,1}};
+			break;
+		case 5:
+			size = 3;
+			position = new Point(3,0);
+			matrix = new int[][] {{0,1,0},
+							      {0,1,0},
+							      {1,1,0}};
+			break;
+		case 6:
+			size = 2;
+			position = new Point(4,0);
+			matrix = new int[][] {{1,1},
+							      {1,1}};
+			break;
+		}
+		
+		
+		
+		
+		refreshLimits();
+	}
+	
+	public void refreshLimits() {
+		//Set Bottom
+		bottom = getRow()+size;
+		boolean continueSearch = true;
+		for (int i = size-1; i >= 0 && continueSearch; i--) {
+			for (int j = 0; j < size; j++) {
+				if (matrix[i][j] == 1) {
+					continueSearch = false;
+					break;
+				}
+				else if (matrix[i][j] == 0 && j == size-1) {
+					bottom--;
+				}
+			}
+		}
+		//Set Left
+		left = getColumn() + 1;
+		continueSearch = true;
+		for (int j = 0; j < size && continueSearch; j++) {
+			for (int i = 0; i < size; i++) {
+				if (matrix[i][j] == 1) {
+					continueSearch = false;
+					break;
+				}
+				else if (matrix[i][j] == 0 && i == size-1) {
+					left++;
+				}
+			}
+		}
+		//Set Right
+		right = getColumn() + size;
+		continueSearch = true;
+		for (int j = size-1; j >= 0 && continueSearch; j--) {
+			for (int i = 0; i < size; i++) {
+				if (matrix[i][j] == 1) {
+					continueSearch = false;
+					break;
+				}
+				else if (matrix[i][j] == 0 && i == size-1) {
+					right--;
+				}
+			}
+		}
+		relocate();
+	}
+	
+	public void rotateLeft(TetrisFrame tetris) {
+		int [][] matrix = new int[size][size];
+		
+		int i2 = 0;
+		int j2 = 0;
+		
+		for (int j = 0; j < size; j++) {
+			for (int i = size-1; i >= 0; i--) {
+				matrix[i][j] = this.matrix[i2][j2];
+				j2++;
+			}
+			j2 = 0;
+			i2++;
+		}
+		
+		boolean canRotate = true;
+		
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
+				try {
+					if (matrix[i][j] == 1 && tetris.getMatrix()[getRow()+i][getColumn()+j] == 1) {
+						canRotate = false;
+					}
+				}
+				catch(Exception e) {}
+			}
+		}
+		
+		if (canRotate) this.matrix = matrix;
+		
+		this.refreshLimits();
+	}
+	
+	public void RotateRight(TetrisFrame tetris) {
+		int [][] matrix = new int[size][size];
+		
+		int i2 = 0;
+		int j2 = 0;
+		
+		for (int j = size-1; j >= 0; j--) {
+			for (int i = 0; i < size; i++) {
+				matrix[i][j] = this.matrix[i2][j2];
+				j2++;
+			}
+			j2 = 0;
+			i2++;
+		}
+		
+		boolean canRotate = true;
+		
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
+				try {
+					if (matrix[i][j] == 1 && tetris.getMatrix()[getRow()+i][getColumn()+j] == 1) {
+						canRotate = false;
+					}
+				}
+				catch(Exception e) {}
+			}
+		}
+		
+		if (canRotate) this.matrix = matrix;
+		
+		refreshLimits();
+	}
+	
+	public void relocate() {
+		while(left<=0) {
+			goRight();
+		}
+		
+		while(right>10) {
+			goLeft();
+		}
+	}
+	
+	public void goDown() {
+		position.setLocation(position.getX(), position.getY()+1);
+		bottom++;
+		moved = true;
+	}
+	
+	public void goLeft() {
+		position.setLocation(position.getX()-1, position.getY());
+		left--;
+		right--;
+	}
+	
+	public void goRight() {
+		position.setLocation(position.getX()+1, position.getY());
+		right++;
+		left++;
+	}
+	
+	public boolean canGoDown(TetrisFrame tetrisFrame) {
+		if(bottom+1 > tetrisFrame.getRows()) {	
+			return false;
+		}
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
+				int[][] matrix = tetrisFrame.getMatrix();
+				if (this.matrix[i][j] == 1) {
+					if(matrix[getRow()+i+1][getColumn()+j] == 1) {
+						return false;
+					}
+				}
+			}
+		}
+		return true;
+	} 
+	
+	public boolean canGoLeft(TetrisFrame tetrisFrame) {
+		if (left-1 <= 0) return false;
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
+				int[][] matrix = tetrisFrame.getMatrix();
+				if (this.matrix[i][j] == 1) {
+					if(matrix[getRow()+i][getColumn()+j-1] == 1) {
+						return false;
+					}
+				}
+			}
+		}
+		return true;
+	}
+	
+	public boolean canGoRight(TetrisFrame tetrisFrame) {
+		if (right+1 > tetrisFrame.getColumns()) return false;
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
+				int[][] matrix = tetrisFrame.getMatrix();
+				if (this.matrix[i][j] == 1) {
+					if(matrix[getRow()+i][getColumn()+j+1] == 1) {
+						return false;
+					}
+				}
+			}
+		}
+		return true;
+	}
+	
+	public int getRow() {
+		return position.y;
+	}
+	
+	public int getColumn() {
+		return position.x;
+	}
+	
+	public int getSize() {
+		return size;
+	}
+	
+	public int getBottom() {
+		return bottom;
+	}
+	
+	public int getLeft() {
+		return left;
+	}
+	
+	public int getRight() {
+		return right;
+	}
+	
+	public int[][] getMatrix() {
+		return matrix;
+	}
+	
+	public boolean getMoved() {
+		return moved;
+	}
+	
+	public Color getColor() {
+		return color;
+	}
+	
+	public void setMoved(boolean b) {
+		moved = b;
+	}
+}
---
+++ Tetris/src/TetrisFrame.java
@@ -0,0 +1,320 @@
+import java.awt.Color;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class TetrisFrame extends JFrame implements KeyListener, ActionListener{
+	private int screenHeight;
+	private int columns;
+	private int height;
+	private int rows;
+	private int squareSize;
+	private int width;
+	private int points;
+	private int speed;
+	private int[][] matrix;
+	private boolean gameOver;
+	private JMenuBar menuBar;
+	private JMenu menuGame;
+	private JMenuItem restartGame;
+	private JMenuItem controls;
+	private ImageIcon icon;
+	private TetrisFrame instance;
+	private JPanel[][] squares;
+	private Tetramino currentTetramino;
+	private Color[][] colorMatrix;
+	public void startGame() {
+		GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+		
+		//Initialize fields
+		
+		gameOver 			= false;
+		menuBar 			= new JMenuBar();
+		menuGame			= new JMenu("Game");
+		restartGame 		= new JMenuItem("Restart Game");
+		controls			= new JMenuItem("Controls");
+		speed				= 1000;
+		screenHeight		= gd.getDisplayMode().getHeight();
+		points 				= 0;
+		instance 			= this;
+		columns				= 10;
+		rows				= 20;
+		squareSize 			= (screenHeight - 100) / rows;
+		height 				= rows * squareSize + 20;
+		width				= columns * squareSize;
+		matrix 				= new int[rows][columns];
+		squares 			= new JPanel[rows][columns];
+		currentTetramino 	= new Tetramino();
+		icon 				= new ImageIcon("src\\t.png");
+		colorMatrix			= new Color[rows][columns];
+		
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < columns; j++) {
+				matrix[i][j] = 0;
+				squares[i][j] = new JPanel();
+			}
+		}
+		
+		//Add components
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < columns; j++) {
+				this.add(squares[i][j]);
+			}
+		}
+		this.setJMenuBar(menuBar);
+		menuBar.add(menuGame);
+		menuGame.add(restartGame);
+		menuGame.add(controls);
+		
+		//Add Action Listener
+		restartGame.addActionListener(this);
+		controls.addActionListener(this);
+		
+		//Configure Frame
+		this.setSize(width,height+20);
+		this.setLayout(new GridLayout(rows,columns));
+		this.setTitle("Tetris");
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.setLocationRelativeTo(null);
+		this.setResizable(false);
+		this.addKeyListener(this);
+		this.setIconImage(icon.getImage());
+				
+		//Set Visibility
+		this.setVisible(true);
+		
+		//Loop
+		Thread loop = new Thread() {
+			@Override
+			public void run() {
+				while (true) {
+					//Refresh Title
+					setTitle("Tetris | Points: " + points);
+					
+					//Clear Completed Rows
+					int rowsCleared = 0;
+					int pointsEarned = 0;
+					
+					for (int i = 0; i < rows; i++) {
+						int counter = 0;
+						for (int j = 0; j < columns; j++) {
+							if (matrix[i][j] == 1) counter++;
+							if (counter == columns) {
+								for (int i2 = i; i2 >= 1; i2--) {
+									for (int j2 = 0; j2 < columns; j2++) {
+										matrix[i2][j2] = matrix[i2-1][j2];
+									}
+								}
+								rowsCleared++;
+								pointsEarned += 100;
+								increaseSpeed();
+							}
+						}
+					}
+					
+					points += pointsEarned * rowsCleared; 
+					
+					//Refresh Output
+					refreshOutput();
+					
+					//Refresh Tetramino Position
+					for (int i = 0; i < currentTetramino.getSize(); i++) {
+						for (int j = 0; j < currentTetramino.getSize(); j++) {
+							if (currentTetramino.getRow() + i < rows || currentTetramino.getColumn() + j < columns) {
+								if (currentTetramino.getMatrix()[i][j] == 1) {
+									while(true) {
+										try {
+											squares[currentTetramino.getRow()+i][currentTetramino.getColumn()+j].setBackground(currentTetramino.getColor());
+											break;
+										}
+										catch(Exception e) {
+											if (currentTetramino.getColumn() >= 5) currentTetramino.goLeft();
+											else currentTetramino.goRight();
+										}
+									}
+								}
+							}
+						}
+					}
+					
+					try {
+						sleep(1000/60);
+					} catch (InterruptedException e) {}
+				}
+			}
+		};
+
+		Thread decreasePoints = new Thread() {
+			@Override
+			public void run() {
+				while (true) {
+					try {
+						sleep(1000);
+					} catch (InterruptedException e) {
+						
+					}
+					if (points - 1 >= 0 && !gameOver) points--;
+				}
+			}
+		};
+		
+		Thread fall = new Thread() {
+			@Override
+			public void run() {
+				while(true) {
+					try {
+						sleep(speed);
+					} catch (InterruptedException e) {}
+
+					if (currentTetramino.canGoDown(instance)) {
+						currentTetramino.goDown();
+					}
+					else {
+						if (currentTetramino.getMoved()) newTetramino();
+						else {
+							gameOver = true;
+							decreasePoints.interrupt();
+							JOptionPane.showMessageDialog(instance, "Points: " + points);
+							restartGame();
+						}
+					}
+				}
+			}
+		};
+		
+		loop.start();
+		fall.start();
+		decreasePoints.start();
+	}
+	
+	public void newTetramino() {
+		for (int i = 0; i < currentTetramino.getSize(); i++) {
+			for (int j = 0; j < currentTetramino.getSize(); j++) {
+				try {
+					if (currentTetramino.getMatrix()[i][j] == 1) colorMatrix[i+currentTetramino.getRow()][j+currentTetramino.getColumn()] = currentTetramino.getColor();
+				}
+				catch(Exception e){}
+				
+				if (currentTetramino.getRow() + i < rows || currentTetramino.getColumn() + j < columns) {
+					if (currentTetramino.getMatrix()[i][j] == 1) {
+						matrix[currentTetramino.getRow()+i][currentTetramino.getColumn()+j] = 1;
+					}
+				}
+			}
+		}
+		currentTetramino = new Tetramino();
+	}
+	
+	public void increaseSpeed() {
+		if (speed - 30 > 0) speed -= 30;
+	}
+	
+	public void restartGame() {
+		points = 0;
+		speed = 1000;
+		currentTetramino = new Tetramino();
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < columns; j++) {
+				matrix[i][j] = 0;
+			}
+		}
+		gameOver = false;
+	}
+	
+	public void refreshOutput() {
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < columns; j++) {
+				if (matrix[i][j] == 0) {
+					squares[i][j].setBackground(new Color(255,255,255));
+				}
+				else if (matrix[i][j] == 1) {
+					squares[i][j].setBackground(colorMatrix[i][j]);
+				}
+			}
+		}
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		currentTetramino.refreshLimits();
+		if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+			if (currentTetramino.canGoRight(this)) {
+				currentTetramino.goRight();
+			}
+		}
+		if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+			if (currentTetramino.canGoLeft(this)) {
+				currentTetramino.goLeft();
+			}
+		}
+		if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+			if (currentTetramino.canGoDown(this)) {
+				currentTetramino.goDown();
+			}
+			else newTetramino();
+		}
+		if (e.getKeyCode() == KeyEvent.VK_Z) {
+			currentTetramino.rotateLeft(this);
+		}
+		if (e.getKeyCode() == KeyEvent.VK_X) {
+			currentTetramino.RotateRight(this);
+		}
+		if (e.getKeyCode() == KeyEvent.VK_C || e.getKeyCode() == KeyEvent.VK_SPACE) {
+			while (currentTetramino.canGoDown(this)) {
+				currentTetramino.goDown();
+			}
+			newTetramino();
+		}
+		if (e.getKeyCode() == KeyEvent.VK_R) {
+			restartGame();
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == restartGame) {
+			restartGame();
+		}
+		else if (e.getSource() == controls) {
+			JOptionPane.showMessageDialog(this,"Arrows Keys: Move\n"+
+											   "Z: Rotate Left\n"+
+											   "X: Rotate Right\n"+
+											   "C or Space: Drop\n"+
+											   "R: Restart Game");
+		}
+	}
+	
+
+	public int getRows() {
+		return rows;
+	}
+	
+	public int getColumns() {
+		return columns;
+	}
+	
+	public int[][] getMatrix() {
+		return matrix;
+	}
+}
